8b9242364a14bdb245af779d04330edc1496cb3f
Shiva Prasad Bandam <trinetra3@gmail.com>
2014-01-28 09:18:13 +0530

    Update JudgementRunner.java
    new runner

--- Judgement/src/main/java/org/mng/mr/util/JudgementRunner.java
+++ Judgement/src/main/java/org/mng/mr/util/JudgementRunner.java
@@ -1,24 +1,61 @@
 package org.mng.mr.util;
 
 import java.util.List;
+import java.util.Random;
 
 import org.mng.mr.domain.Card;
+import org.mng.mr.domain.Face;
+import org.mng.mr.domain.Judgement;
 import org.mng.mr.domain.Player;
 
 public class JudgementRunner {
 
-	Card[] cardsDeck = Card.values();
+	private Card[] cardsDeck = Card.values();
 
-	private void schuffleCards() {
-
+	private void shuffleCards() {
+		Random rand = new Random();
+		int shuffle = rand.nextInt(15);
+		
 	}
 
 	private void distributeCards(int cards, List<Player> listOfPlayers) {
 
+		int iterator = 0;
+		for (int i = 0; i < cards; i++) {
+			for (Player player : listOfPlayers) {
+				iterator++;
+				player.appendCard(cardsDeck[iterator]);
+			}
+		}
+
 	}
 
-	private void startRound() {
+	private void startRound(int cardsCount, List<Player> listOfPlayers) {
+		shuffleCards();
+		distributeCards(cardsCount, listOfPlayers);
+		startPlay();
+
+	}
+
+	private void startPlay() {
+
+	}
+
+	private void startGame(int playersCount) {
+
+		Judgement jud = new Judgement(playersCount, Face.HEART);
+
+		int round = jud.getRounds();
+
+		for (int i = 0; i < 2 * round - 1; i++) {
+			startRound(i + 1, jud.getPlayerList());
+		}
+
+	}
+
+	public static void main(String args[]) {
 
 	}
 
 }
+
